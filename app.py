3f9524a73a3ed3e6a80601624d2a1776c4a2a19b
Era <80454009+Lerory123Ay@users.noreply.github.com>
2024-12-18 12:49:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -163,8 +163,4 @@ def internal_server_error(e):
 # Application Entry Point
 if __name__ == '__main__':
     create_tables()
-    app.run(
-        host='0.0.0.0', 
-        port=int(os.environ.get('PORT', 5000)), 
-        debug=os.environ.get('FLASK_DEBUG', 'False') == 'True'
-    )
+    app.run(host='0.0.0.0')

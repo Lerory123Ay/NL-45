c25f387da058bfa5d795a512426eea89e0e051fe
Era <80454009+Lerory123Ay@users.noreply.github.com>
2024-12-18 15:15:43 +0530

    Update NewsletterEmail.js

--- newsletter-app/models/NewsletterEmail.js
+++ newsletter-app/models/NewsletterEmail.js
@@ -1,1 +1,10 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
 
+const NewsletterEmail = sequelize.define('NewsletterEmail', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  createdAt: { type: DataTypes.DATE, defaultValue: Sequelize.NOW }
+});
+
+module.exports = NewsletterEmail;

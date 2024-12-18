06a270cc271b9e45191182d2325f079ed4a84ab6
Era <80454009+Lerory123Ay@users.noreply.github.com>
2024-12-18 15:48:35 +0530

    Update db.js

--- newsletter-app/config/db.js
+++ newsletter-app/config/db.js
@@ -1,9 +1,12 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  logging: false
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 module.exports = sequelize;

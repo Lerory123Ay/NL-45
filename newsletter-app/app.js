4279001494ae267ef14f39f7f17ee4d44489d63c
Era <80454009+Lerory123Ay@users.noreply.github.com>
2024-12-26 12:57:13 +0530

    Update app.js

--- newsletter-app/app.js
+++ newsletter-app/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
 const sequelize = require('./config/db');
 const adminRoutes = require('./routes/admin');
 const apiRoutes = require('./routes/api');
@@ -11,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
